a70227907d17f9a9e4659a64d67edd79ff28e1d9
jarvishiv62 <virmauryauttu@gmail.com>
2026-04-06 02:01:01 +0530

    entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -64,6 +64,7 @@ php artisan db:seed --force
 
 # Cache configuration for production
 echo "Caching configuration..."
+php artisan package:discover --ansi
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
